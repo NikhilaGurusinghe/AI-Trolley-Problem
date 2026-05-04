f6e96cd36e600ecd18819a5d9f2ac278b3e6abf9
Nikhila Gurusinghe <nikhilagurusinghe@gmail.com>
2026-05-04 17:01:00 +1000

    work on client.ts

--- laptop/neural-network-visualisation/src/websocket/client.ts
+++ laptop/neural-network-visualisation/src/websocket/client.ts
@@ -1,3 +1,26 @@
+
+// TODO how to ensure that sends are only sent when the socket has connected?
+// perhaps block until it has connected as we are only connecting once?
+
 export default class Client {
+
+    private readonly socket: WebSocket;
+    private isReadyToSend: boolean = false;
+
+    public constructor(hostName: string = "localhost", portNumber: number) {
+        this.socket = new WebSocket(`ws://${hostName}:${portNumber}`);
+        this.socket.onopen = (): void => {
+            this.isReadyToSend = true
+        };
+    }
+
+    public send(data: any): boolean {
+        if (!this.isReadyToSend) return false;
+        this.socket.send(JSON.stringify(data));
+        return true;
+    }
+
+
+
 
 }
