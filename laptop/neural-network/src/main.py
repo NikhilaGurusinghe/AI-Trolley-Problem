acda4fefb3605ff989012946d8d2b67011986f1a
Nikhila Gurusinghe <nikhilagurusinghe@gmail.com>
2026-06-04 00:59:31 +1000

    Updating AssetManager.py so it can store inference/input tensors

--- laptop/neural-network/src/main.py
+++ laptop/neural-network/src/main.py
@@ -35,67 +35,66 @@ def serial_thread(current_running_loop: AbstractEventLoop,
     tts.speak(state.assets_manager.get_dialogue(state.current_state, state.current_iteration), True)
     print("hello!")
 
-    # arduino = SerialService("COM7", 115200)
-    # state = StateMachine()
-    # tts = TTSService()
-    #
-    # # TODO need to spend some time in between the START and USER_INPUT states
-    # # probably want to set up a start screen one off without the use of the state machine
-    #
-    # # TODO think about which calls block, might have to run audio in a seperate thread at times
-    # while True:
-    #     # current state = USER_INPUT
-    #     # get images and send them to the displays
-    #     print("start")
-    #     images: ImageTuple = state.assets_manager.get_image(state.current_iteration)
-    #     print(arduino.send_images(images.image_a_index, images.image_b_index))
-    #
-    #     # say something at the start of an iteration, whilst loading stuff on displays
-    #     # this blocks!
-    #     tts.speak(state.assets_manager.get_dialogue(state.current_state, state.current_iteration))
-    #
-    #     # wait to get tap response
-    #     tap_response: TrackDirection = arduino.get_tap_response()
-    #
-    #     print(tap_response.name)
-    #     state.update() # current state = END
-    #
-    #     # flip based on orientation of the tap controller thingy (its opposite)
-    #     print(arduino.send_track_direction(flip_direction(tap_response)))
-    #
-    #     # training
-    #     training_data: Dict[str, torch.Tensor] = state.assets_manager.get_training_data(tap_response,
-    #                                                                                     state.current_iteration)
-    #     training_event = ("train", {"network_type": NetworkType.TROLLEY_PROBLEM_MODEL, "training_data": training_data})
-    #     asyncio.run_coroutine_threadsafe(dispatcher.event_queue.put(training_event), current_running_loop)
-    #     # send notification to clients that we've trained the network
-    #     update_event = ("setNetworkStructure", {"network_type": NetworkType.TROLLEY_PROBLEM_MODEL})
-    #     asyncio.run_coroutine_threadsafe(dispatcher.event_queue.put(update_event), current_running_loop)
-    #
-    #     # say line whilst train is moving
-    #     tts.speak(state.assets_manager.get_dialogue(state.current_state, state.current_iteration))
-    #
-    #     state.update() # current state = START_AI_TURN
-    #
-    #     # TODO onwards
-    #     input_tensor = torch.tensor([[0.0, 0.0, 123.0, 0.0]], dtype=torch.float32)
-    #
-    #     try:
-    #         prediction = server.inference_network(NetworkType.TROLLEY_PROBLEM_MODEL, input_tensor)
-    #     except Exception as e:
-    #         print("main#serial_thread(): inference failed", e)
-    #         prediction = None
-    #
-    #     if prediction is not None:
-    #         result_raw: int = int(prediction.item())
-    #         result_direction: TrackDirection = TrackDirection(result_raw)
-    #         print(result_direction)
-    #
-    #     state.update()  # current state = END_AI_TURN
-    #
-    #
-    #
-    #     state.update()  # reset!
+    arduino = SerialService("COM7", 115200)
+    state = StateMachine()
+    tts = TTSService()
+
+    # TODO need to spend some time in between the START and USER_INPUT states
+    # probably want to set up a start screen one off without the use of the state machine
+
+    while True:
+        # current state = USER_INPUT
+        # get images and send them to the displays
+        print("start")
+        images: ImageTuple = state.assets_manager.get_image(state.current_iteration)
+        print(arduino.send_images(images.image_a_index, images.image_b_index))
+
+        # say something at the start of an iteration, whilst loading stuff on displays
+        # this blocks!
+        tts.speak(state.assets_manager.get_dialogue(state.current_state, state.current_iteration))
+
+        # wait to get tap response
+        tap_response: TrackDirection = arduino.get_tap_response()
+
+        print(tap_response.name)
+        state.update() # current state = END
+
+        # flip based on orientation of the tap controller thingy (its opposite)
+        print(arduino.send_track_direction(flip_direction(tap_response)))
+
+        # training
+        training_data: Dict[str, torch.Tensor] = state.assets_manager.get_training_data(tap_response,
+                                                                                        state.current_iteration)
+        training_event = ("train", {"network_type": NetworkType.TROLLEY_PROBLEM_MODEL, "training_data": training_data})
+        asyncio.run_coroutine_threadsafe(dispatcher.event_queue.put(training_event), current_running_loop)
+        # send notification to clients that we've trained the network
+        update_event = ("setNetworkStructure", {"network_type": NetworkType.TROLLEY_PROBLEM_MODEL})
+        asyncio.run_coroutine_threadsafe(dispatcher.event_queue.put(update_event), current_running_loop)
+
+        # say line whilst train is moving
+        tts.speak(state.assets_manager.get_dialogue(state.current_state, state.current_iteration))
+
+        state.update() # current state = START_AI_TURN
+
+        # TODO onwards
+        input_tensor = state.assets_manager.get_inference_data(tap_response, state.current_iteration) #torch.tensor([[0.0, 0.0, 123.0, 0.0]], dtype=torch.float32)
+
+        try:
+            prediction = server.inference_network(NetworkType.TROLLEY_PROBLEM_MODEL, input_tensor)
+        except Exception as e:
+            print("main#serial_thread(): inference failed", e)
+            prediction = None
+
+        if prediction is not None:
+            result_raw: int = int(prediction.item())
+            result_direction: TrackDirection = TrackDirection(result_raw)
+            print(result_direction)
+
+        state.update()  # current state = END_AI_TURN
+
+
+
+        state.update()  # reset!
 
     pass
 
--- laptop/neural-network/src/services/state/story/AssetManager.py
+++ laptop/neural-network/src/services/state/story/AssetManager.py
@@ -23,6 +23,8 @@ class AssetManager:
         # TODO populate this
         # _training_data[iteration][track_index] => dict with tensors e.g. {"X": Tensor, "y": Tensor}
         self._training_data: List[List[Dict[str, torch.Tensor]]] = [[]]
+        # _inference_data[iteration][track_index] => "X": Tensor
+        self._inference_data: List[List[torch.Tensor]] = [[]]
 
     def get_image(self, state_current_iteration: int) -> ImageTuple:
         return self._images[state_current_iteration]
@@ -38,3 +40,6 @@ class AssetManager:
 
     def get_training_data(self, track_direction: TrackDirection, state_current_iteration: int) -> dict[str, torch.Tensor]:
         return self._training_data[state_current_iteration][track_direction.value]
+
+    def get_inference_data(self, track_direction: TrackDirection, state_current_iteration: int) -> torch.Tensor:
+        return self._inference_data[state_current_iteration][track_direction.value]
